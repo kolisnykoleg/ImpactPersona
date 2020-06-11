1d3ff80ec96d3fb4d8348baeef148ffc3519bc84
Oleg <kolisnykoleg@gmail.com>
2020-06-11 10:12:16 +0300

    Checkout results

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -8,4 +8,18 @@ class Checkout extends CI_Controller
         $this->load->view('checkout');
         $this->load->view('footer');
     }
+
+    public function complete()
+    {
+        $this->load->view('header');
+        $this->load->view('transaction_complete');
+        $this->load->view('footer');
+    }
+
+    public function failed()
+    {
+        $this->load->view('header');
+        $this->load->view('transaction_failed');
+        $this->load->view('footer');
+    }
 }
